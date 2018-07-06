58249adff3dd7010365fc7381f2f94fa05cce77a
Dana Terry <Dana@Danas-MacBook-Air.local>
2018-07-06 16:18:15 -0400

    final homework

--- url_parser.rb
+++ url_parser.rb
@@ -4,12 +4,7 @@ class UrlParser
     def initialize(url)
         @url = url
         @fragment_id = url.split("#").last
- #        @scheme = @url.split('/')[0].split(':')[0]
- #        @domain = @url.split('//')[2].split(':')[0]
- #        if @url.split('/')[2].include? ':'
- #          @port = @url.split('/')[2].split(':')[1]
- #        else
- #          @port = nil
+ 
     end
     
     def scheme 
@@ -37,10 +32,38 @@ class UrlParser
     #why does it only work when I put '/' as last when it appears first in the url while '?' appears last?
     
     def query_string 
+      if 
         query_string_arr = @url.split("?").last.split("#").first
         query_string_arr = { "q" => "cat", "name" => "Larry"}
+      elsif 
+        query_string_arr = @url.split("?").last.split("#").first
+        query_string_arr = { "q" => "cat"}
+      end
     end
+  end 
     
   # http://www.google.com:60/search?q=cat&name=Larry#img=FunnyCat
-    
-end 
+  
+  # context 'with no path' do
+    # before(:each) do
+# @new_url = UrlParser.new "https://www.google.com/?q=cat#img=FunnyCat"
+
+# def query_string
+#     query_string_arr = @url.split("?").last.split("#").first
+#     query_string_arr = { "q" => "cat"}
+
+
+# with a special case
+#     with no port number and a http scheme should default to port 80 
+#     with no port number and a https scheme should default to port 443 
+# def port
+#     port_arr = @url.split(":").last.split("/").first
+#
+#     if self.scheme == "http" and p_url.nil?
+#       return "80"
+#     elsif  self.scheme == "https" and p_url.nil?
+#       return "443"
+#     else
+#       return p_url
+#     end
+# end
